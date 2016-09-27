4aff2b3f9d59ca3b4f1f30c26d3968f1401ec83d
James Kong <k.kong@actimirror.com>
2016-09-27 11:43:00 +0800

    update

--- src/MyVideoPlayer.cpp
+++ src/MyVideoPlayer.cpp
@@ -10,66 +10,66 @@
 //needs implementing
 bool MyVideoPlayer::load(string name) {
     player.load(name);
-};
+}
 void MyVideoPlayer::loadAsync(string name){
     player.loadAsync(name);
-};
+}
 
 void MyVideoPlayer::play(){
     player.play();
-};
+}
 void MyVideoPlayer::stop(){
     player.stop();
-};
+}
 
 bool MyVideoPlayer::isPaused(){
     return player.isPaused();
-};
+}
 bool MyVideoPlayer::isLoaded(){
     return player.isLoaded();
-};
+}
 bool MyVideoPlayer::isPlaying(){
     return player.isPlaying();
-};
+}
 //should implement!
 float MyVideoPlayer::getPosition(){
     return player.getPosition();
-};
+}
 
 float MyVideoPlayer::getDuration(){
     return player.getDuration();
-};
+}
 bool MyVideoPlayer::getIsMovieDone(){
     return player.getIsMovieDone();
-};
+}
 
 void MyVideoPlayer::setLoopState(ofLoopType state){
     player.setLoopState(state);
-};
+}
 void MyVideoPlayer::setSpeed(float speed){
     player.setSpeed(speed);
-};
+}
 
 float MyVideoPlayer::getWidth(){
     return player.getWidth();
-};
+}
 
 float MyVideoPlayer::getHeight(){
     return player.getHeight();
-};
+}
 
 bool MyVideoPlayer::isFrameNew(){
     return player.isFrameNew();
-};
+}
 void MyVideoPlayer::close(){
     player.close();
-};
+}
 
 void MyVideoPlayer::draw(ofRectangle rect){
     player.draw(rect);
-};
+}
 
 
 void MyVideoPlayer::update(){
     player.update();
-};
+}
